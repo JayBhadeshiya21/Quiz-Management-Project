4a3275d40a1bafe8a0e7370c52e034cb7fc30e64
w3schools-test <bhadeshiyajay12@gmail.com>
2025-02-14 21:43:14 +0530

    Add project files.

---
+++ Controllers/QuestionLevelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz1.Controllers
+{
+    public class QuestionLevelController : Controller
+    {
+        public IActionResult ListQuestionLevel()
+        {
+            return View();
+        }
+
+        public IActionResult AddQuestionLevel()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/QuizWiseQuestionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz1.Controllers
+{
+    public class QuizWiseQuestionController : Controller
+    {
+
+        public IActionResult ListQuizWiseQuestions()
+        {
+            return View();
+        }
+
+        public IActionResult AddQuizWiseQuestions()
+        {
+            return View();
+        }
+    }
+}
