832f1410bc7be1e7ccb6cfb8a2b9593b04571994
w3schools-test <bhadeshiyajay12@gmail.com>
2025-02-28 22:09:43 +0530

    select all and delete

--- Controllers/QuizWiseQuestionController.cs
+++ Controllers/QuizWiseQuestionController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Quiz1.Controllers
 {
     public class QuizWiseQuestionController : Controller
     {
+        private IConfiguration configuration;
+
+        public QuizWiseQuestionController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
 
         public IActionResult ListQuizWiseQuestions()
         {
-            return View();
+            string connectionString = this.configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_QuizWiseQuestions_SelectAll";
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            return View(table);
         }
 
         public IActionResult AddQuizWiseQuestions()
